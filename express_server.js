3677fe7cc192fe4975a3d359464e034397f97184
Lauren Johnston <hilotraining@outlook.com>
2023-01-23 13:58:53 -0700

    added another event handler path for /urls.json

--- express_server.js
+++ express_server.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 //instructions for what to do when server starts up
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
