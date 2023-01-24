97164ac164209c38ee61bb2402361d9caf25e344
Lauren Johnston <hilotraining@outlook.com>
2023-01-24 13:57:20 -0700

    restored edits

--- express_server.js
+++ express_server.js
@@ -1,42 +1,60 @@
+//
+//CONFIGURE APP
+//
 const express = require('express');
 const app = express(); //func to call server object
 const PORT = 8080;
-
 app.set("view engine", "ejs");
-//middleware will parse the body of POST request from a buffer to a string and make it readable
-app.use(express.urlencoded({ extended: true }));
 
+
+//
+//MIDDLEWARE
+//
+app.use(express.urlencoded({ extended: true })); //parse the body of POST request from a buffer to a string and make it readable
+
+
+//
+//DATABASE
+//
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//route handler to get and render the urls_new template
+
+//
+//ROUTES
+//
+
+//get and render the urls_new template
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//route handler to pass the single URL data to whow.ejs
+//pass the single URL data to display a single URL and its shortened form
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+//redirect to long url link
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
-//route handler to pass the URL data to our template.
+//pass the URL data (urlDatabase) to urls_index template.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//generate shortURL  (callback)
 const generateRandomString = () => {
   return Math.random().toString(36).slice(-6);
 };
-//logs the request body and gives a dummy form response.
+
+//add new url to database, redirect to short url page
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL; //update database
@@ -44,25 +62,36 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/:${shortURL}`);
 });
 
-//delete request
+//delete url from database, redirct user to main page
 app.post("/urls/:id/delete", (req, res) => {
-  //delete urlDatabase
-})
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+  console.log(shortURL);
+});
 
-//route handler
+//edit url
+app.post("/urls/edit/:id", (req, res) => {
+  //const shortURL = req.params.id; //update database
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(req.body); // Log the POST request body to the console
+  res.render(`urls_edit`,templateVars);
+});
+
+//route handler to home. can i delete?
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-//route handler
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//route handler
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// //route handler
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 
 //instructions for what to do when server starts up
 app.listen(PORT, () => {
